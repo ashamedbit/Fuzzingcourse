569656420995463a2050e9b72ea87f5ac8691c38
Aniruddhan Murali <a25mural@Aniruddhans-MacBook-Air.local>
2023-09-22 02:28:43 -0400

    Add Red black tree assertion checkers and fuzzers

---
+++ fuzzing/RBVerify.py
@@ -0,0 +1,80 @@
+from RBTree import *
+
+# Define R-B Tree
+class RBVerify():
+    tree_depth = -1
+
+    # Root is black and has no parent
+    def root_assertion(self, root):
+        assert root.color == 0
+        assert root.parent == None
+
+    # tree nodes are either red or black
+    def red_black(self, root):
+        if root == None:
+            return
+        assert root.color == 0 or root.color == 1
+        self.red_black(root.left)
+        self.red_black(root.right)
+
+    # tree is acyclic
+    def acyclic(self, root):
+        visited = set()
+        if root == None:
+            return
+        if root not in visited:
+            visited.add(root)
+        else:
+            assert "CYCLIC!"
+        self.acyclic(root.left)
+        self.acyclic(root.right)
+    
+    #  All leafs are black
+    def leaf_is_black(self, root):
+        if root == None:
+            return
+        if root.left == None and root.right == None:
+            assert root.color == 0
+        self.leaf_is_black(root.left)
+        self.leaf_is_black(root.right)
+
+    # A red node cannot have a red node as a child
+    def red_property(self, root):
+        if root == None:
+            return
+        if root.color == 1:
+            if root.left is not None and root.left.color == 1:
+                assert "No two consecutive reds!"
+            if root.right is not None and root.right.color == 1:
+                assert "No two consecutive reds!"
+        self.red_property(root.left)
+        self.red_property(root.right)
+    
+    # Number of black nodes on each path from root to leaf is same
+    def depth_property(self, root, depth):
+        if root == None:
+            return
+        if root.color == 0:
+            depth = depth + 1
+        
+        if root.left == None and root.right == None:
+            # Set this static variable for first time
+            if RBVerify.tree_depth == -1:
+                RBVerify.tree_depth = depth
+            if RBVerify.tree_depth != depth:
+                assert "Unequal black nodes on different paths!"
+        self.depth_property(root.left, depth)
+        self.depth_property(root.right, depth)
+
+    def check_assertions(self, tree):
+        root = tree.root
+        #Insert code to check assertions below
+        self.root_assertion(root)
+        self.red_black(root)
+        self.acyclic(root)
+        self.leaf_is_black(root)
+        self.red_property(root)
+        self.depth_property(root, 0)
+        return True
+
+
